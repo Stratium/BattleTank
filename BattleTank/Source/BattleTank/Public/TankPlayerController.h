b3be4c1528b9ccb4d3fae8c32794f1d88004a38c
Tony Faye <tony_faye735@live.co.uk>
2018-01-27 23:20:09 +0000

    167 Created binding for crosshair colour

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -13,15 +13,16 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
-	ATank* GetControlledTank() const;	
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	ATank* GetControlledTank() const;
 
+private:
 	virtual void BeginPlay() override;
 	/*	BeginPlay is defined in AActor, the parent of our player controller. Virtual means that children of a class can override this method down the inheritance tree.
 		We therefore COPY AActor's BeginPlay function (virtual and all) and then add override to ensure this version of BeginPlay is being read by the compiler.		*/
 	virtual void Tick(float DeltaTime) override;
 
-private:
 	void AimTowardsCrosshair();
 
 	// Getters
