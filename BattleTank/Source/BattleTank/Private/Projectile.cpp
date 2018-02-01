f0666afe3b6f39f1d388b95695eff39c324873c8
Tony Faye <tony_faye735@live.co.uk>
2018-02-01 21:06:21 +0000

    193 Projectiles now have an impact blast

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -5,15 +5,21 @@
 
 AProjectile::AProjectile()
 {
-	PrimaryActorTick.bCanEverTick = true; // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false; // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh")); // Sets a default (unremovable) component on Projectile_BP
 	SetRootComponent(CollisionMesh); // Makes our component the root component
 	CollisionMesh->SetNotifyRigidBodyCollision(true); // Automatically checks "Simulation generates hit events"
 	CollisionMesh->SetVisibility(false); // Automatically unchecks "Visible" in rendering
 
+	// TODO Update AttachTo to new API
+
 	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Launch Blast")); // Sets a default (unremovable) component on Projectile_BP
-	LaunchBlast->AttachTo(RootComponent); // Attaches it to our root component, in this case our collision mesh
+	LaunchBlast->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform); // Attaches it to our root component, in this case our collision mesh
+
+	ImpactBlast = CreateDefaultSubobject<UParticleSystemComponent>(FName("Impact Blast")); // Sets a default (unremovable) component on Projectile_BP
+	ImpactBlast->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform); // Attaches it to our root component, in this case our collision mesh
+	ImpactBlast->bAutoActivate = false;
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement")); // Sets a default (unremovable) movement component on Tank_BP
 	ProjectileMovement->bAutoActivate = false;
@@ -22,15 +28,17 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-}
-
-void AProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	CollisionMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
 }
 
 void AProjectile::LaunchProjectile(float Speed)
 {
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	LaunchBlast->Deactivate();
+	ImpactBlast->Activate();
 }
--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -22,14 +22,16 @@ protected:
 
 public:	
 	AProjectile(); // Sets default values for this actor's properties
-	virtual void Tick(float DeltaTime) override;
-
 	void LaunchProjectile(float Speed);
 
 private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* CollisionMesh = nullptr; // Used to detect when our projectile hits things
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent* ImpactBlast = nullptr;
 };	
